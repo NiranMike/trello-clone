640cdf8dfd1af6f1a17ff732e9a8c1be93cbf103
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-06-04 13:14:13 +0100

    style: styled header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Board from "@/components/Board";
+import Header from "@/components/Header";
+
 
 export default function Home() {
   return (
     <main className="">
       
+      <Header />
+      <Board />
     </main>
   )
 }
